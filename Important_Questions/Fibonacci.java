aa12380d06df4bae02b7752ba78973e61dd28ac5
“Arunraj003” <arunr6506@gmail.com>
2024-04-18 20:07:51 +0530

    commit "Files added"

--- Important_Questions/Fibonacci.java
+++ Important_Questions/Fibonacci.java
@@ -5,8 +5,15 @@ public class Fibonacci {
     public static void main(String args[]){
         // 3. Write a program to find fibonacci seriesof given number
         Scanner scan = new Scanner(System.in);
-        
-
+        System.out.println("Enter the number ");
+        int n = scan.nextInt();
+        int a=-1,b=1,c;
+        for(int i=1;i<=n;i++){
+            c=a+b; // 0
+            System.out.println(c);
+            a=b;  // 1
+            b=c; // 0
+        }
         scan.close();
     }
 }
